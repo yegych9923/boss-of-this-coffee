9392376b62f5ae38d2bb9798216c125029a86511
yegych9923 <egorknyazev10@gmail.com>
2022-04-26 20:55:07 +0400

    commit 4

--- js/main.js
+++ js/main.js
@@ -1,2 +1,10 @@
+$(document).ready(function() {
+    $('body').bind('mousemove', function(e) {
+        var coeff = 30;
+        var x = -1 * (e.clientX / coeff);
+        var y = -1 * (e.clientY / coeff);
 
-$('#hero-img').mo
+        document.getElementById('hero-img').style.marginLeft = x + 'px';
+        document.getElementById('hero-img').style.marginTop = y + 'px';
+    });
+});
