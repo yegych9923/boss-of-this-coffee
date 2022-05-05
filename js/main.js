8ab3dce415a598ae3ec36cb7e264c3855f7ef9f1
yegych9923 <egorknyazev10@gmail.com>
2022-05-05 19:51:05 +0400

    commit 5

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 $(document).ready(function() {
+    const fontSize = Number(window.getComputedStyle(document.body).getPropertyValue('font-size').match(/\d+/)[0])
+    const screenWidth = document.body.clientWidth;
+
+    function rem(size) {
+        return size / fontSize;
+    }
+
     $('body').bind('mousemove', function(e) {
         var coeff = 30;
         var x = -1 * (e.clientX / coeff);
@@ -7,4 +14,9 @@ $(document).ready(function() {
         document.getElementById('hero-img').style.marginLeft = x + 'px';
         document.getElementById('hero-img').style.marginTop = y + 'px';
     });
+
+    if (rem(screenWidth) > rem(1200)) {
+        document.getElementsByClassName('diskounts-catalog-items').style.rowGap = 
+            (screenWidth - $('.catalog__item').css('width') * 4) / 4;
+    }
 });
